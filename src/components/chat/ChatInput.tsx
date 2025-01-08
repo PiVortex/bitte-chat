d31588d59783445af700919fe7cfea82d964a6e4
ruisantiago <ruisantiagomr@gmail.com>
2025-01-08 14:21:41 +0000

    wip

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -83,7 +83,7 @@ export const SmartActionsInput = ({
       <Button
         type='submit'
         disabled={!input || isLoading}
-        className='w-full h-[42px] lg:w-[42px] p-0 disabled:opacity-20'
+        className='h-[42px] lg:w-[42px] p-0 disabled:opacity-20'
         style={{ backgroundColor: buttonColor, color: textColor }}
       >
         <ArrowUp className='h-[16px] w-[16px] hidden lg:block' />
