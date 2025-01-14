4f8fa46a33359930076d5feb4e4a127ead6aeff1
Ruben Marcus <ruben@rubenmarcus.dev>
2025-01-14 16:55:53 +0000

    ts-config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  darkMode: "class",
+  prefix: "bitte-",
   theme: {
     extend: {
       screens: {
@@ -13,5 +14,6 @@ module.exports = {
       },
     },
   },
+  darkMode: "class",
   plugins: [],
-};
+} satisfies Config;
