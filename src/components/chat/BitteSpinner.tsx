42c5246f3a84560b93691a488c6ba5b9c249dd8d
ruisantiago <ruisantiagomr@gmail.com>
2025-01-16 16:24:29 +0000

    add filter calculation

--- src/components/chat/BitteSpinner.tsx
+++ src/components/chat/BitteSpinner.tsx
@@ -1,6 +1,21 @@
 import Lottie from "react-lottie-player/dist/LottiePlayerLight";
 import bitteAnimation from "./../../assets/bitte_animation.json";
 
+const hexToRgb = (hex: string) => {
+  const bigint = parseInt(hex.slice(1), 16);
+  const r = (bigint >> 16) & 255;
+  const g = (bigint >> 8) & 255;
+  const b = bigint & 255;
+  return { r, g, b };
+};
+
+const calculateFilter = (r: number, g: number, b: number) => {
+  const invert = 1 - r / 255;
+  const sepia = g / 255;
+  const saturate = b / 255;
+  return `invert(${invert}) sepia(${sepia}) saturate(${saturate})`;
+};
+
 export const BitteSpinner = ({
   width = 200,
   height = 200,
@@ -8,23 +23,19 @@ export const BitteSpinner = ({
   width?: number;
   height?: number;
 }) => {
+  const { r, g, b } = hexToRgb('#FF0000');
+  const filter = calculateFilter(r, g, b);
+
+  console.log({filter})
+
   return (
-    <div style={{ filter: `hue-rotate(#FF0000)` }}>
+    <div>
       <Lottie
         loop
         animationData={bitteAnimation}
         play
         speed={1.5}
-        style={{ width, height }}
-        rendererSettings={{
-          preserveAspectRatio: "xMidYMid slice",
-          filterSize: {
-            width: "150%",
-            height: "150%",
-            x: "-25%",
-            y: "-25%",
-          },
-        }}
+        style={{ width, height, filter }}
       />
     </div>
   );
