24e5bde70d5c14b942337ecb599e0a75a2aa5398
Luís Freitas <microchipgnu@gmail.com>
2025-01-23 16:28:46 +0000

    test omit credentials

--- src/components/chat/ChatContent.tsx
+++ src/components/chat/ChatContent.tsx
@@ -164,7 +164,7 @@ export const ChatContent = ({
     },
     sendExtraMessageFields: true,
     initialMessages,
-    credentials: "include",
+    credentials: "omit",
     headers: {
       Authorization: `Bearer ${apiKey}`,
     },
