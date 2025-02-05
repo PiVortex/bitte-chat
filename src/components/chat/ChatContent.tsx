ebf7f5c5a7cac3c01c686a373ad13dbefa845341
ruisantiago <ruisantiagomr@gmail.com>
2025-02-05 17:50:11 +0000

    add evm address to chat message sender if no accountId

--- src/components/chat/ChatContent.tsx
+++ src/components/chat/ChatContent.tsx
@@ -12,6 +12,7 @@ import React, {
 import { Hex } from "viem";
 import { defaultColors } from "../../lib/constants";
 import { BITTE_IMG } from "../../lib/images";
+import { executeLocalToolCall } from "../../lib/local-agent";
 import { cn } from "../../lib/utils";
 import {
   AssistantsMode,
@@ -24,7 +25,6 @@ import { Button } from "../ui/button";
 import { BitteSpinner } from "./BitteSpinner";
 import { SmartActionsInput } from "./ChatInput";
 import { MessageGroup } from "./MessageGroup";
-import { executeLocalToolCall, executeToolCall } from "../../lib/local-agent";
 
 export const ChatContent = ({
   agentId,
@@ -40,7 +40,7 @@ export const ChatContent = ({
   const [autoScrollEnabled, setAutoScrollEnabled] = useState(true);
   const messagesRef = useRef<HTMLDivElement | null>(null);
 
-  const { accountId, evmAddress, chainId, evmWallet } = useAccount();
+  const { accountId, evmAddress, chainId } = useAccount();
 
   const {
     borderColor,
@@ -221,7 +221,7 @@ export const ChatContent = ({
                     chatId={chatId}
                     key={groupKey}
                     groupKey={groupKey}
-                    accountId={accountId!}
+                    accountId={accountId || evmAddress || ""}
                     messages={messages}
                     isLoading={isInProgress}
                     messageBackgroundColor={messageBackground!}
