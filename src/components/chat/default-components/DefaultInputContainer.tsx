46171a06be062935bc20b4c3ef9b4cf72094ef52
ruisantiago <ruisantiagomr@gmail.com>
2025-02-26 13:13:44 +0000

    add defautl input container

---
+++ src/components/chat/default-components/DefaultInputContainer.tsx
@@ -0,0 +1,15 @@
+import { InputContainerProps } from "../../../types";
+
+const DefaultInputContainer = ({ children, style }: InputContainerProps) => (
+  <div
+    className='lg:bitte-rounded-md bitte-border-t bitte-border-b lg:bitte-border bitte-p-6 bitte-w-full'
+    style={{
+      backgroundColor: style.backgroundColor,
+      borderColor: style.borderColor,
+    }}
+  >
+    {children}
+  </div>
+);
+
+export default DefaultInputContainer;
